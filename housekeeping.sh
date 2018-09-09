89c1fd21084c96aaf65bc3ab6d1fe633c38134aa
Adamo Tonete <adamobr@users.noreply.github.com>
2018-09-09 16:21:12 -0300

    Created housekeeping.sh

---
+++ housekeeping.sh
@@ -0,0 +1,23 @@
+sudo rm -fR /data/
+
+sudo mkdir /data
+
+sudo rm -fR /var/lib/cassandra/hints/*
+sudo rm -fR /var/lib/cassandra/saved_caches/*
+sudo rm -fR /var/lib/cassandra/commitlog/*
+
+sudo mkdir -p /data/lib/cassandra/hints/
+sudo mkdir -p /data/lib/cassandra/saved_cache/
+sudo mkdir -p /data/lib/cassandra/commitlog/
+
+sudo yum install xfsprogs.x86_64 -y
+
+sudo mkfs.xfs ${mount_path}
+
+sudo mount %mount_path% /data
+
+sudo chown -R cassandra.cassandra /data
+
+echo "%mount_path%       /data   xfs    defaults,nofail" | sudo tee --append /etc/fstab
+
+sudo mount -a
