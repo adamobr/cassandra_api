b5f044518ce25ed889b855062444f8f214619501
Adamo Tonete <adamobr@users.noreply.github.com>
2018-09-09 16:37:23 -0300

    fixed mount path

--- housekeeping.sh
+++ housekeeping.sh
@@ -12,12 +12,12 @@ sudo mkdir -p /data/lib/cassandra/commitlog/
 
 sudo yum install xfsprogs.x86_64 -y
 
-sudo mkfs.xfs %mount_path%
+sudo mkfs.xfs %mount_path
 
-sudo mount %mount_path% /data
+sudo mount %mount_path /data
 
 sudo chown -R cassandra.cassandra /data
 
-echo "%mount_path%       /data   xfs    defaults,nofail" | sudo tee --append /etc/fstab
+echo "%mount_path       /data   xfs    defaults,nofail" | sudo tee --append /etc/fstab
 
 sudo mount -a
